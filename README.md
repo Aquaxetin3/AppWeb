4391d3d27b59b8b21d79e7644dd9175f75d72d03
Dayana <dayanalara95@gmail.com>
2017-09-12 10:04:04 -0600

    Firebase Auth and Db management

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# AppWeb
-aplicacionweb
+# Ionic 2 Demo with Firebase Auth and Database Usage
+
+This is a sample of Ionic v2 and Firebase Auth and DB Connection. 
+You can signup and login to the system then you can add your notes like to-do app.
+
+Firebase Auth System is used for Signup and Login.
+Firebase DB is used for adding notes.
+
+I have used [Ionic v2 Conferance App](https://github.com/driftyco/ionic-conference-app) for template.
+
+Other than Firebase, I have used Google Maps API for a simple Google Maps Demo from Conferance App.
+
+## Getting Started
+
+Firstly, update myFirebaseConfig in app.module.ts for your own settings.
+```
+$ git clone https://github.com/mg52/Ionic-v2-Firebase
+$ cd Ionic-v2-Firebase
+$ npm install
+$ ionic serve
+```
+
+ 
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,70 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-import { LandingPage } from '../pages/landing/landing';
+import { NgModule } from '@angular/core';
 
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/storage';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
- var config = {
-    apiKey: "AIzaSyBan7VaLzMJ6aoN3kMjN_58Jr5KCdPt54k",
+import { Ionic_2_Firebase } from './app.component';
+
+import { ShowDataPage } from '../pages/show-data/show-data';
+import { EnterDataPage } from '../pages/enter-data/enter-data';
+import { DataPopoverPage } from '../pages/data-popover/data-popover';
+import { AccountPage } from '../pages/account/account';
+import { LoginPage } from '../pages/login/login';
+import { MapPage } from '../pages/map/map';
+import { SignupPage } from '../pages/signup/signup';
+import { TabsPage } from '../pages/tabs/tabs';
+import { DataDetailPage } from '../pages/data-detail/data-detail';
+
+import {  MapData } from '../providers/map-data';
+import { UserData } from '../providers/user-data';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+
+export const myFirebaseConfig = {
+     apiKey: "AIzaSyBan7VaLzMJ6aoN3kMjN_58Jr5KCdPt54k",
     authDomain: "photoapp-1ca9a.firebaseapp.com",
     databaseURL: "https://photoapp-1ca9a.firebaseio.com",
     projectId: "photoapp-1ca9a",
     storageBucket: "photoapp-1ca9a.appspot.com",
     messagingSenderId: "880869713676"
-  };
+};
+
+
+export const myFirebaseAuthConfig = {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Password
+};
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage,
-    ListPage,
-    LandingPage
+    Ionic_2_Firebase,
+    EnterDataPage,
+    ShowDataPage,
+    AccountPage,
+    LoginPage,
+    MapPage,
+    DataPopoverPage,
+    SignupPage,
+    TabsPage,
+    DataDetailPage
   ],
   imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-        AngularFireModule.initializeApp(config),
-        AngularFireAuthModule
+    IonicModule.forRoot(Ionic_2_Firebase),
+		IonicStorageModule.forRoot(),
+    AngularFireModule.initializeApp(myFirebaseConfig, myFirebaseAuthConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage,
-    LandingPage
+    Ionic_2_Firebase,
+    EnterDataPage,
+    ShowDataPage,
+    AccountPage,
+    LoginPage,
+    MapPage,
+    DataPopoverPage,
+    SignupPage,
+    TabsPage,
+    DataDetailPage
   ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+  providers: [MapData, UserData]
 })
-export class AppModule {}
+export class AppModule { }
